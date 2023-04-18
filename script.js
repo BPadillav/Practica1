ee58593b650d4284a4f5bf8b3bf5d1f249df8d27
BPadillav <130064038+BPadillav@users.noreply.github.com>
2023-04-17 23:15:54 -0500

    3 files modified

--- script.js
+++ script.js
@@ -1,5 +1,56 @@
 console.log('Hello!');
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
 
 
+// Función para validar el formulario
+function validarFormulario() {
+  // Obtener los valores de los campos del formulario
+  var nombre = document.getElementById('nombre').value;
+  var email = document.getElementById('email').value;
+  var mensaje = document.getElementById('mensaje').value;
 
+  // Validar que el nombre no esté vacío
+  if (nombre === '') {
+    document.getElementById('errorNombre').innerText =
+      'Por favor ingrese su nombre';
+    return false;
+  } else {
+    document.getElementById('errorNombre').innerText = '';
+  }
+
+  // Validar que el email sea válido
+  var emailRegEx = /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/;
+  if (!email.match(emailRegEx)) {
+    document.getElementById('errorEmail').innerText =
+      'Por favor ingrese un email válido';
+    return false;
+  } else {
+    document.getElementById('errorEmail').innerText = '';
+  }
+
+  // Validar que el mensaje no esté vacío
+  if (mensaje === '') {
+    document.getElementById('errorMensaje').innerText =
+      'Por favor ingrese un mensaje';
+    return false;
+  } else {
+    document.getElementById('errorMensaje').innerText = '';
+  }
+
+  // Enviar el formulario si todos los campos son válidos
+  return true;
+}
+
+// Función para cambiar el tamaño de la letra
+function cambiarTamanioLetra(tamanio) {
+  document.body.style.fontSize = tamanio + "px";
+}
+
+// Función para cambiar el color de fondo
+function cambiarColorFondo(color) {
+  document.body.style.backgroundColor = color;
+}
+
+// Función para cambiar el tipo de letra
+function cambiarTipoLetra(tipo) {
+  document.body.style.fontFamily = tipo + ", sans-serif";
+}
